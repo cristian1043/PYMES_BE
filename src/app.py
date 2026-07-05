9f814a5b348c0f54e9d2bb76b474d2e6ea9ac705
DUBONACO <garavitojimenezcristian@gmail.com>
2026-07-05 16:55:20 -0500

    apply changes

--- src/app.py
+++ src/app.py
@@ -2,25 +2,33 @@ from flask import Flask
 
 from src.models import Base, engine
 
+#importar los modelos para que se registren en la base de datos
+
 from src.models.categorias import Categorias
 from src.models.clientes import Clientes
 from src.models.productos import Productos
 from src.models.proveedores import Proveedores
 from src.models.roles import Roles
 from src.models.usuarios import Usuarios
+from src.models.metodos_pago import MetodosPago
 from src.models.facturas import Facturas
 from src.models.detalle_facturas import DetalleFacturas
 from src.models.compras import Compras
 from src.models.detalle_compras import DetalleCompras
 
+#crear todas las tablas en la base de datos
 
 Base.metadata.create_all(bind=engine)
 
 app = Flask(__name__)
 
-@app.route("/")
-def inicio():
-    return "Backend funcionando"
+#importar las rutas de la aplicación
+
+from src.routes.home import home
+
+#registrar las rutas de la aplicación
+
+app.register_blueprint(home)
 
 if __name__ == '__main__':
     app.run(debug=True)
