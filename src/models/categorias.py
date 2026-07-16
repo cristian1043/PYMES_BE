4d52fac94c5630358194c98dfda8f13df8420d54
DUBONACO <garavitojimenezcristian@gmail.com>
2026-07-16 13:45:48 -0500

    yes

--- src/models/categorias.py
+++ src/models/categorias.py
@@ -7,27 +7,27 @@ class Categorias(Base):
     id = Column(Integer, primary_key=True)
     nombre = Column(String(100), nullable=False)
 
-def save(self):
-    session.add(self)
-    session.commit()
+    def save(self):
+        session.add(self)
+        session.commit()
 
-@staticmethod
-def get():
-    return session.query(Categorias).all()
+    @staticmethod
+    def get():
+        return session.query(Categorias).all()
 
-@staticmethod
-def get_by_id(id):
-    return session.query(Categorias).filter_by(id=id).first()
+    @staticmethod
+    def get_by_id(id):
+        return session.query(Categorias).filter_by(id=id).first()
 
-def update(self):
-    session.commit()
+    def update(self):
+        session.commit()
     
-def delete(self):
-    session.delete(self)
-    session.commit()
-    
-def to_dict(self):
-    return {
-        "id": self.id,
-        "nombre": self.nombre
-    }
+    def delete(self):
+        session.delete(self)
+        session.commit()
+   
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre
+        }
